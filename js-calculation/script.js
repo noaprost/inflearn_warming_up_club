0c02260822a67ca27b595e9bcf0754a46ec3e09b
noaprost <kangjiwon1850@gmail.com>
2024-10-03 14:38:40 +0900

    js math,  study

---
+++ js-calculation/script.js
@@ -0,0 +1,24 @@
+const num1 = 20;
+const num2 = 10;
+
+let val;
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.E;
+val = Math.PI;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.4);
+val = Math.abs(-2);
+val = Math.min(1, 2, 3, -4);
+val = Math.max(1, 2, 3, -4);
+
+// 1 ~ 20
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
